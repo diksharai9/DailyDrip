179eb8689f34245b8dab3654e6a3e5745582a119
diksharai9 <maildiksharai9@gmail.com>
2025-07-26 22:19:10 +0530

    feat:req file updated

--- minimal_calendar/tasks/views.py
+++ minimal_calendar/tasks/views.py
@@ -9,6 +9,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
 
 
 @api_view(['GET'])
@@ -46,7 +47,7 @@ def update_task(request, task_id):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+@csrf_exempt
 @api_view(['POST'])
 def register_user(request):
     username = request.data.get("username")
